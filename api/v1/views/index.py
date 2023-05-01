a232ea822cc24bd6904ea1dc44e938aa2f185e48
alexUd01 <alexanderikpeama@gmail.com>
2023-05-01 09:49:34 +0100

    Task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,18 @@
 """Create route"""
 from api.v1.views import app_views
 import json
+from flask import make_response
 
 
 @app_views.route('/status')
 def status():
     """Return api status"""
-    return json.dumps({'status': 'OK'}) + '\n'
+    resp = make_response(json.dumps({'status': 'OK'}, indent=2) + '\n')
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+
+
+@app_views.route('/stats')
+def stats():
+    """Return api stat"""
+    
