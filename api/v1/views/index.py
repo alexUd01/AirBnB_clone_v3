ec07608c75a02a2c8e2ff39dddc77f3ef1fc2634
alexUd01 <alexanderikpeama@gmail.com>
2023-05-01 09:27:41 +0100

    Task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create route"""
+from api.v1.views import app_views
+import json
+
+
+@app_views.route('/status')
+def status():
+    """Return api status"""
+    return json.dumps({'status': 'OK'})
