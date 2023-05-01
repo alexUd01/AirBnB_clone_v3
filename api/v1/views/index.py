8403a8bce489d5a70994610de7c7e78caa2a79fa
alexUd01 <alexanderikpeama@gmail.com>
2023-05-01 10:14:31 +0100

    Task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,20 @@
 from api.v1.views import app_views
 import json
 from flask import make_response
+from models import storage
+
+
+def prettify(dict_obj):
+    """prettily serializes dictionary objects to json string"""
+    if not isinstance(dict_obj, dict):
+        raise TypeError('"dict_object" must be a python dictionary')
+    return json.dumps(dict_obj, indent=2) + "\n"
 
 
 @app_views.route('/status')
 def status():
     """Return api status"""
-    resp = make_response(json.dumps({'status': 'OK'}, indent=2) + '\n')
+    resp = make_response(prettify({'status': 'OK'}))
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
@@ -16,4 +24,14 @@ def status():
 @app_views.route('/stats')
 def stats():
     """Return api stat"""
-    
+    stat = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    resp = make_response(prettify(stat))
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
