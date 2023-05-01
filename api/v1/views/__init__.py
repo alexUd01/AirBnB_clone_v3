61c9793b84df672f9d05ccf2ebbb1fa6169b7256
alexUd01 <alexanderikpeama@gmail.com>
2023-05-01 16:08:41 +0100

    Task 6: State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,4 @@ app_views = Blueprint(name='app_views', import_name=__name__,
                       url_prefix='/api/v1')
 
 from api.v1.views.index import *
-from api.v1.views.states import *
+from api.v1.views.states import states
